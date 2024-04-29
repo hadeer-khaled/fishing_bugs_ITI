a9364e55028af9bc8b413e47b321b78d91cf5b53
hadeer-khaled <hadeer.awad02@eng-st.cu.edu.eg>
2024-04-29 16:26:39 +0300

    Solved: return all authors

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,7 +3,8 @@ class AuthorsController < ApplicationController
 
   # GET /authors or /authors.json
   def index
-    @authors = Author.find(params[:id])
+    # @authors = Author.find(params[:id])
+    @authors = Author.all
   end
 
   # GET /authors/1 or /authors/1.json
